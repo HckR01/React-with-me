4964803dafa7a6f67a51bbfb71da8a80ee675cf4
HckR01 <139985239+HckR01@users.noreply.github.com>
2025-05-30 11:12:07 +0530

    07 project rout in React

---
+++ 07ReactRouter/src/main.jsx
@@ -0,0 +1,33 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import Home from './components/Home/Home.jsx'
+import About from './components/About/About.jsx'
+// import Layout from './Layout.jsx'  // ✅ Add this line
+
+import Layout from './layout.jsx' // ✅ Use the correct path for your Layout component
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Layout />,
+    children: [
+      {
+        path: '',
+        element: <Home />
+      },
+      {
+        path: 'about',
+        element: <About />
+      }
+    ]
+  }
+])
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <RouterProvider router={router} />
+  </StrictMode>
+)
