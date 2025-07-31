f30bdfd851ac5a0a5d7f88725d6d1a5bb2628df0
HckR01 <139985239+HckR01@users.noreply.github.com>
2025-07-31 12:01:36 +0530

    done pizza menu

--- pizza-menu/src/index.js
+++ pizza-menu/src/index.js
@@ -1,12 +1,150 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import './index.css';
+//data of the pizza ...............................
+const pizzaData = [
+  {
+    name: 'Focaccia',
+    ingredients: 'Bread with italian olive oil and rosemary',
+    price: 6,
+    photoName: 'pizzas/focaccia.jpg',
+    soldOut: false,
+  },
+  {
+    name: 'Pizza Margherita',
+    ingredients: 'Tomato and mozarella',
+    price: 10,
+    photoName: 'pizzas/margherita.jpg',
+    soldOut: false,
+  },
+  {
+    name: 'Pizza Spinaci',
+    ingredients: 'Tomato, mozarella, spinach, and ricotta cheese',
+    price: 12,
+    photoName: 'pizzas/spinaci.jpg',
+    soldOut: false,
+  },
+  {
+    name: 'Pizza Funghi',
+    ingredients: 'Tomato, mozarella, mushrooms, and onion',
+    price: 12,
+    photoName: 'pizzas/funghi.jpg',
+    soldOut: false,
+  },
+  {
+    name: 'Pizza Salamino',
+    ingredients: 'Tomato, mozarella, and pepperoni',
+    price: 15,
+    photoName: 'pizzas/salamino.jpg',
+    soldOut: true,
+  },
+  {
+    name: 'Pizza Prosciutto',
+    ingredients: 'Tomato, mozarella, ham, aragula, and burrata cheese',
+    price: 18,
+    photoName: 'pizzas/prosciutto.jpg',
+    soldOut: false,
+  },
+];
+
+//..........................................
 
 function App() {
-  return <h1>hello</h1>;
+  return (
+    <div className="container">
+      <Header />
+      <Menu />
+      <Footer />
+    </div>
+  );
+}
+
+/**
+ * A functional component that renders a heading with the text "hello".
+ */
+//components.....................................
+function Header() {
+  const style = {};
+  return (
+    <header className="header">
+      <h1 style={style}>Fast React Pizza Co.</h1>
+    </header>
+  );
+}
+
+function Footer() {
+  const hours = new Date().getHours();
+  const openHours = 9;
+  const closeHours = 22;
+  const isOpen = hours >= openHours && hours <= closeHours;
+
+  return (
+    <footer className="footer">
+      {isOpen ? (
+        <div className="order">
+          <p>
+            We're open until {closeHours}:00. Come visit us or order online!
+          </p>
+          <butto className="btn">Order here</butto>
+        </div>
+      ) : (
+        <p>Sorry, we're closed. We open at {openHours}:00.</p>
+      )}
+    </footer>
+  );
+}
+
+function Menu() {
+  return (
+    <main className="menu">
+      <h2>Our Menu</h2>
+      <ul className="pizzas">
+        {pizzaData.map((pizza) => (
+          <Pizza
+            name={pizza.name}
+            ingredients={pizza.ingredients}
+            price={pizza.price}
+            photoname={pizza.photoName}
+            key={pizza.name}
+          />
+        ))}
+      </ul>
+
+      {/* <Pizza
+        name="Pizza spina"
+        ingredients="Tomato, mozarella, spinach,myserect  and ricotta cheese"
+        photoname="/pizzas/spinaci.jpg"
+        price={12}
+      />
+      <Pizza
+        name="Pizza Margherita"
+        ingredients="Tomato, mozarella, spinach,myserect  and ricotta cheese"
+        photoname="/pizzas/margherita.jpg"
+        price={12}
+      /> */}
+    </main>
+  );
+}
+
+function Pizza(props) {
+  return (
+    <li className="pizza">
+      <img src={props.photoname} alt="focaccia" />
+      <div>
+        <h3>{props.name}</h3>
+        <p>{props.ingredients}</p>
+        <span>{props.price + 3}$</span>
+      </div>
+    </li>
+  );
 }
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
