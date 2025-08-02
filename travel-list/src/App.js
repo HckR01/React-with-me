906809bd966b5e40d56706bd3c5b716fe6fbe995
HckR01 <139985239+HckR01@users.noreply.github.com>
2025-08-02 20:05:39 +0530

    update creating ui add all thing now just some work

--- travel-list/src/App.js
+++ travel-list/src/App.js
@@ -1,13 +1,70 @@
-export default AudioParamMap(){
+const initialItems = [
+  { id: 1, description: 'Passports', quantity: 2, packed: false },
+  { id: 2, description: 'Socks', quantity: 12, packed: true },
+  { id: 3, description: 'Charger', quantity: 1, packed: false },
+];
 
+export default function App() {
+  return (
+    <div className="app">
+      <Logo />
+      <Form />
+      <PackingList />
+      <State />
+    </div>
+  );
 }
-function Logo(){
-  return <h1>🌴FAR AWAY🥥</h1>
+function Logo() {
+  return <h1>🌴FAR AWAY🥥</h1>;
 }
-function Form(){
-  return <div className="add-form">
-    <h3>What do you need for yoy </h3>
-  </div>
+function Form() {
+  return (
+    <form className="add-form">
+      <h3>❤️What do you need for your trip? 🤔 </h3>
+      <select>
+        {Array.from({ length: 20 }, (_, i) => (
+          <option key={i + 1} value={i + 1}>
+            {i + 1}
+          </option>
+        ))}
+      </select>
+      <input
+        type="text"
+        placeholder="items...
+      "
+      />
+
+      <button>Add</button>
+    </form>
+  );
 }
-function PackingList(){}
-function state(){}
+function PackingList() {
+  return (
+    <div className="list">
+      <ul>
+        {initialItems.map((item) => (
+          <Item item={item} key={item.id} />
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+function Item({ item }) {
+  return (
+    <li>
+      <span style={item.packed ? { textDecoration: 'line-through' } : {}}>
+        {item.quantity}
+        {item.description}
+      </span>
+      <button>❌</button>
+    </li>
+  );
+}
+function State() {
+  return (
+    <footer className="stats">
+      <em>💼you have X item On your list,And you are already packed X ✅</em>
+    </footer>
+  );
+}
