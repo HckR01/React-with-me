e0e08e370b08202e09adfacd8097bb38a8d27b96
HckR01 <139985239+HckR01@users.noreply.github.com>
2025-08-02 18:57:26 +0530

    update creating ui

---
+++ travel-list/src/App.js
@@ -0,0 +1,13 @@
+export default AudioParamMap(){
+
+}
+function Logo(){
+  return <h1>🌴FAR AWAY🥥</h1>
+}
+function Form(){
+  return <div className="add-form">
+    <h3>What do you need for yoy </h3>
+  </div>
+}
+function PackingList(){}
+function state(){}
